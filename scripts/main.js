b743d07e42575328cf1bd9fda5950a42bdec5954
Hadrien <hadrien.chevallier71@gmail.com>
2017-07-22 11:23:13 +0200

    Loadin, Rotation, Scale
    Ajout des fonctionnalité du loading, de rotation des sprite ainsi que le
    scale

--- scripts/main.js
+++ scripts/main.js
@@ -36,6 +36,7 @@ loader
 		"images/blob.png",
 		"images/door.png",
 		"images/treasure.png"])
+	.on("progress", loadProgressHandler)
 	.load(setup);
 
 /*
@@ -47,16 +48,44 @@ function setup() {
 	let spriteDoor = new Sprite(TextureCache["images/door.png"]);
 	let spriteTreasure = new Sprite(TextureCache["images/treasure.png"]);
 
+	// Set sprite position
+	spriteExplorer.x = 96;
+	spriteExplorer.y = 128;
+	spriteTreasure.anchor.x = 0.5;
+	spriteTreasure.anchor.y = 0.5;
+	spriteExplorer.rotation = 0.5;
+
+	spriteBlob.position.set(235, 52);
+	spriteBlob.width = 80;
+	spriteBlob.height = 120;
+	spriteBlob.anchor.set(0.5, 0.5);
+	spriteBlob.rotation = getRadiansFromDegrees(45);
+
+	spriteDoor.position.set(125, 200);
+	spriteDoor.scale.x = 0.5;
+	spriteDoor.scale.y = 0.5;
+	spriteDoor.pivot.set(spriteDoor.width / 2, spriteDoor.height / 2);
+	spriteDoor.rotation = getRadiansFromDegrees(90);
+
+	spriteTreasure.position.set(352, 110);
+	spriteTreasure.scale.set(2, 2);
+
 	stage.addChild(spriteExplorer);
 	stage.addChild(spriteBlob);
 	stage.addChild(spriteDoor);
 	stage.addChild(spriteTreasure);
 
 	// Make sprites disappear
-	spriteTreasure.visible = false;
+	// spriteTreasure.visible = false;
 
 	// Tell the renderer to render the stage
 	renderer.render(stage);
+}
+
+function loadProgressHandler(loader, resource) {
+	console.log("Loading: " + resource.url);
+
+	console.log("Progress: " + loader.progress);
 }
 
 
